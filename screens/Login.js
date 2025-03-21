30b9e90616dd36a0694415f2d31a20d342fe4391
ENRIQUE FLORES <flores.gallardo.luisenrique1@gmail.com>
2025-03-20 23:05:23 -0600

    Login1

--- screens/Login.js
+++ screens/Login.js
@@ -1,14 +1,62 @@
 import { Component } from "react";
-import { StyleSheet,View,Text } from 'react-native';
+import { StyleSheet,View,Text, TextInput} from 'react-native';
+import { Image } from 'react-native';
 
 export default function Login(){
+
 {
     return (
-        <View>
-            <Text>Login</Text>
+        <View style={styles.padre}>
+            
+            <View>
+                <Image source={require('../Assets/img/Logo.jpeg')} style={styles.profile} />
+            </View>
+            <View>
+                <Text style={styles.textit}>Inicio de Sesión</Text>
+            </View>
+            <View style={styles.card}>
+                <View style={styles.cajatext}>
+                    <TextInput placeholder="Usuario o Correo" style={{paddingHorizontal:15}} />
+                </View>
+                <View style={styles.cajatext}>
+                    <TextInput placeholder="Contraseña" style={{paddingHorizontal:15}} />
+                </View>
+            </View>
         </View>
     )
 }
 }
 
-const styles = StyleSheet.create ({})
+const styles = StyleSheet.create ({
+    padre: {
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'white'
+    },
+
+    profile: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderColor: 'white'
+    },
+    card: {
+        margin:20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        width:'90%',
+        padding: 20,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity:0.25,
+        shadowRadius:4,
+        elevation:5
+    },
+    textit: {
+        fontSize: 24,
+        color: 'purple',
+}})
